948f4adb756b5fdd8dc3c4251b95607aa8d40129
vikasyadu2603 <118845699+vikasyadu2603@users.noreply.github.com>
2022-11-25 20:05:44 +0530

    change sevrice

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -9,26 +9,27 @@ export class ApiService {
 
   constructor(private http : HttpClient) { }
   postEmploye(data :any){
-    return this.http.post<any>("https://github.com/vikasyadu2603/AngularCrudOperation/blob/master/db.json/posts/", data)
+    return this.http.post<any>("http://localhost:3000/posts", data)
     .pipe(map((res:any)=>{
       return res;
     }))
   }
   getEmploye(){
-    return this.http.get<any>("https://github.com/vikasyadu2603/AngularCrudOperation/blob/master/db.json/posts/")
+    return this.http.get<any>("http://localhost:3000/posts"
+    )
     .pipe(map((res:any)=>{
       return res;
     }))
   }
   deleteEmploye(id :number){
-    return this.http.delete<any>("https://github.com/vikasyadu2603/AngularCrudOperation/blob/master/db.json/posts/"+id)
+    return this.http.delete<any>("http://localhost:3000/posts/"+id)
     .pipe(map((res:any)=>{
       return res;
      
     }))
   }
   updateEmploye(data :any ,id: number){
-    return this.http.put<any>("https://github.com/vikasyadu2603/AngularCrudOperation/blob/master/db.json/posts/"+id , data)
+    return this.http.put<any>("http://localhost:3000/posts/"+id , data)
     .pipe(map((res:any)=>{
       return res;
     }))
